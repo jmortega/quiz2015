b1d10f07f003e23e7b432d8d9bf2db058ecd0a5b
jortegac <jortegac@jortegac>
2015-06-07 17:41:24 +0200

    commit quiz with author

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET autor page. */
+router.get('/author', function(req, res, next) {
+  res.render('author', { title: 'Autor' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
