269bedbce06a04a52e55067c8f8830388e98e371
jortegac <jortegac@jortegac>
2015-06-13 20:51:56 +0200

    quiz search

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,55 @@
 
+var models = require('../models/models.js');
+
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta});
+  })
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-  var resultado = 'Roma';
-  if (req.query.respuesta === resultado) {
+   var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
     resultado = 'Correcto';
-  }else{
-	resultado = 'Incorrecto';
   }
-  res.render('quizes/answer', {respuesta: resultado});
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
 
+
+// Autoload - factoriza el código si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
+};
+
+// GET /quizes
+exports.index = function(req, res) {
+
+  //Obtener parámetro de busqueda
+  var search = req.query.search || "";
+
+  //filtro para búsqueda mediante expresion regular
+  search = "%" + search.replace(/\s/gi, "%") + "%";
+
+  var _paramSearch = {
+    where: ["pregunta like ?", search]
+  };
+  
+  models.Quiz.findAll(_paramSearch).then(function(quizes) {
+    res.render('quizes/index', { quizes: quizes});
+   }).catch(function(error) { next(error);})
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+
+    res.render('quizes/show', { quiz: req.quiz});
+  
+};
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
@@ -15,5 +18,10 @@ router.get('/author', function(req, res, next) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
